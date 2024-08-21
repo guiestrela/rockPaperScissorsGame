bb6109e27cb97ab7be6e84f073fe581926e6f6ae
Guilherme Estrela Lambert de Brito <guilhermestrela@gmail.com>
2024-08-21 18:21:32 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER WINS';
 
 let gameIsRunning = false;
 
@@ -20,6 +23,25 @@ const getPlayerChoice = function () {
         return selection;
 };
 
+const getComputerChoice = function () {
+    const choices = [ROCK, PAPER, SCISSORS];
+    return choices[Math.floor(Math.random() * choices.length)];
+};
+
+const getWinner = function (cChoice, pChoice) {
+    if (cChoice === pChoice) {
+        return RESULT_DRAWER; 
+    } else if (
+        cChoice === ROCK && pChoice === PAPER || 
+        cChoice === PAPER && cChoice === SCISSORS ||
+        cChoice === SCISSORS && pChoice === ROCK 
+    ) {
+        return RESULT_PLAYER_WINS; 
+    } else {
+        return RESULT_COMPUTER_WINS; 
+    }
+};
+
 startGameBtn.addEventListener('click', function() {
     if (gameIsRunning) {
         return;
@@ -28,4 +50,8 @@ startGameBtn.addEventListener('click', function() {
     console.log('Game is starting...');
     const playerSelection = getPlayerChoice();
     console.log(playerSelection);
+    const computerChoice = getComputerChoice();
+    console.log(computerChoice);
+    const winner = getWinner(computerChoice, playerSelection);
+    console.log(`The winner is: ${winner}`);
 });
